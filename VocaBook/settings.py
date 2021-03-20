a5b6fe9f2fdd17cee60090ee2ae1478bc3134b2c
sandeepyadav07780 <sandeep.yadav.07780@gmail.com>
2021-03-21 02:03:12 +0530

    Updated STATIC_ROOT path

--- VocaBook/settings.py
+++ VocaBook/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 

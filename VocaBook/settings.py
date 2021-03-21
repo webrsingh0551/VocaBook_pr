7b0df9f8afc95e9f33843b2496d899881b077715
sandeepyadav07780 <sandeep.yadav.07780@gmail.com>
2021-03-21 19:13:55 +0530

    Changed SECRET_KEY to environment variable SECRET_KEY

--- VocaBook/settings.py
+++ VocaBook/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gnjam!fl2(^!089p#*p2ay7i)7=mxq2nf4&9olij&dpknm5n(%'
+#SECRET_KEY = 'gnjam!fl2(^!089p#*p2ay7i)7=mxq2nf4&9olij&dpknm5n(%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

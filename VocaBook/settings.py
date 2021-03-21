e3c286efd75bf906a67628bdd0e0bf03ab55a646
sandeepyadav07780 <sandeep.yadav.07780@gmail.com>
2021-03-21 18:49:43 +0530

    changed DEBUG=True

--- VocaBook/settings.py
+++ VocaBook/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'gnjam!fl2(^!089p#*p2ay7i)7=mxq2nf4&9olij&dpknm5n(%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 

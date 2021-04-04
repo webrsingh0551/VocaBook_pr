1b1623a9b8635c3f56a2cfae028b2eb9b210aa7a
sandeepyadav07780 <sandeep.yadav.07780@gmail.com>
2021-04-04 22:53:37 +0530

    Message

--- VocaBook/settings.py
+++ VocaBook/settings.py
@@ -21,14 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'gnjam!fl2(^!089p#*p2ay7i)7=mxq2nf4&9olij&dpknm5n(%'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'gnjam!fl2(^!089p#*p2ay7i)7=mxq2nf4&9olij&dpknm5n(%'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [   'vocabookapp01.herokuapp.com',
-                    'desolate-mountain-71654.herokuapp.com']
+                    'desolate-mountain-71654.herokuapp.com',
+                    '127.0.0.1']
 
 
 # Application definition
